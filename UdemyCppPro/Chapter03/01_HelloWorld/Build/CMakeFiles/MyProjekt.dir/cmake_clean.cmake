48d8f57619750b067876815e7cae672be77ae561
Carsten Fehlhaber <c.fehlhaber@genion.de>
2023-12-19 14:48:37 +0100

    Udemy Ordner hinzugefügt: 20231219

---
+++ UdemyCppPro/Chapter03/01_HelloWorld/Build/CMakeFiles/MyProjekt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProjekt.dir/main.cc.o"
+  "CMakeFiles/MyProjekt.dir/main.cc.o.d"
+  "MyProjekt"
+  "MyProjekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProjekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
