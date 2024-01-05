6ed247af0ae06b78c7c957ac0410e2223fb79ed4
Carsten Fehlhaber <c.fehlhaber@genion.de>
2024-01-05 15:55:05 +0000

    Update 20240105-1700

--- UdemyCpp/00_Test/src/Test.cpp
+++ UdemyCpp/00_Test/src/Test.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hallo Carsten";
+    int intZahl;
+    // unsind int unsigndZahl;
+    double doubleZahl;
+
+    char buchstabe;
+    string einString;
+
+
+    std::cout << "Int Zahl?" << std::endl;
+    std::cin >> intZahl;
+    std::cout << sizeof(intZahl) << std::endl;
+    
+    std::cout << "Double Zahl?" << std::endl;
+    std::cin >> doubleZahl;
+    std::cout << sizeof(doubleZahl) << std::endl;
+
+    std::cout << "Char?" << std::endl;
+    std::cin >> buchstabe;
+    std::cout << sizeof(buchstabe) << std::endl;
+
+    std::cout << "String?" << std::endl;
+    std::cin >> einString;
+    std::cout << sizeof(einString) << std::endl;
 }
